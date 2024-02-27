aed88de095a7eb8ec3b267a04d65b1caf8724ecf
xThoorop <xthoorop@gmail.com>
2024-02-27 20:38:31 +0100

    poprawa wygladu

--- src/day_27_02_2024/zadanie_5/SetInterface.java
+++ src/day_27_02_2024/zadanie_5/SetInterface.java
@@ -1,13 +1,6 @@
 package day_27_02_2024.zadanie_5;
 
 public interface SetInterface<V> {
-    /*
-                • add
-• remove
-• size
-• contains
-• clear
-     */
 
     void add(V value);
     void remove(V value);
