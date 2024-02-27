155a4927703c8b80a0fa56d6c69f186677971d0c
xThoorop <xthoorop@gmail.com>
2024-02-27 20:40:54 +0200

    zadanie_4

--- src/zadanie_4/Storage.java
+++ src/zadanie_4/Storage.java
@@ -5,16 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/*
-ZADANIE 4.
-Stwórz klasę Storage, która będzie miała prywatne pole typu Map, publiczny konstruktor oraz metody:
-addToStorage(String key, String value) → dodawanie elementów do przechowywalni
-printValues(String key) → wyświetlanie wszystkich elementów pod danym kluczem
-findValues(String value) → wyświetlanie wszystkich kluczy, które mają podaną wartość
-Klasa Storage powinna pozwalać na przechowywanie wielu wartości pod jednym kluczem.
-Bonus:
-Napisz rozwiązanie aby przyjmowało dowolny typ generyczny zamiast String key i value;
- */
 public class Storage<K,V> {
 
 
@@ -32,7 +22,6 @@ public class Storage<K,V> {
             this.map.put(key, new Values<V>().addValues(value));
         }
     }
-//findValues(String value) → wyświetlanie wszystkich kluczy, które mają podaną wartość
 
     public void findValues(V values) {
         for (Map.Entry<K, Values> entry : this.map.entrySet()) {
