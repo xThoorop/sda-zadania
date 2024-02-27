2826e4a27c4454fd70053ff116be4abd2a4c44d5
xThoorop <xthoorop@gmail.com>
2024-02-27 19:03:55 +0200

    1 zadanie

---
+++ src/zadanie_1/Main.java
@@ -0,0 +1,36 @@
+package zadanie_1;
+
+
+import java.util.*;
+
+public class Main {
+
+
+    /*
+    Zadanie 1:
+Stwórz metodę, która jako parametr przyjmuje listę stringów, następnie zwraca tą listę posortowaną alfabetycznie od Z do A.
+Poszukaj odpowiednich metod do sortowania w klasach:
+Collections (static method)
+List (non-static method)
+Poszukaj potrzebnych obiektów Comparator w klasach:
+Comparator
+Collections
+String
+     */
+
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("a","b","c","d","z");
+        sortFromZToA(list);
+        list.stream().forEach(System.out::println);
+    }
+
+
+    public static List<String> sortFromZToA(List<String> list) {
+        Collections.sort(list);
+        Collections.reverse(list);
+        return list;
+    }
+
+
+    }
