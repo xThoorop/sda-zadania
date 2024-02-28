2139b7bee5271e2db326bd9842c2c0b0a9fb8c1e
xThoorop <xthoorop@gmail.com>
2024-02-28 19:12:48 +0100

    update

--- src/day_28_02_2024/zadanie_8/validators/Validator.java
+++ src/day_28_02_2024/zadanie_8/validators/Validator.java
@@ -5,19 +5,6 @@ import day_28_02_2024.zadanie_8.objects.Parcel;
 import java.util.function.Function;
 
 public class Validator {
-    /*
-    ZADANIE 8.
-Zaimplementuj interfejs Validator, który będzie zawierać w swojej deklaracji metodę boolean validate(Parcel input). Stwórz klasę Parcel z parametrami:
-• int xLength
-• int yLength
-• int zLength
-• float weight
-• boolean isExpress
-Validator powinien weryfikować czy suma wymiarów (x, y, z) nie
-przekracza 300, czy każdy z rozmiarów nie jest mniejszy niż 30,
-czy waga nie przekracza 30.0 dla isExpress=false lub 15.0 dla isExpress=true.
-W przypadku błędów, powinien o nich poinformować użytkownika.
-     */
 
 
     public boolean validate(Parcel parcel,boolean showMessages) {
